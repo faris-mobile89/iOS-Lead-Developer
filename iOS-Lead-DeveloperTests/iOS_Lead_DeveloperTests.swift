87f8618d5e01f660317eb143056f5f90a7fc4d80
Faris <outsource.fn@tra.gov.ae>
2020-02-25 14:52:47 +0400

    Initial Commit

---
+++ iOS-Lead-DeveloperTests/iOS_Lead_DeveloperTests.swift
@@ -0,0 +1,34 @@
+//
+//  iOS_Lead_DeveloperTests.swift
+//  iOS-Lead-DeveloperTests
+//
+//  Created by Faris Abu Saleem on 25/02/2020.
+//  Copyright © 2020 Faris Abu Saleem. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_Lead_Developer
+
+class iOS_Lead_DeveloperTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
